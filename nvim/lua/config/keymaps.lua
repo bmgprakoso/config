edd8bcdf4d345d72698e1677bcaa392ca4f1eebd
Bambang Adi Prakoso <bambang.prakoso@agriaku.com>
2025-05-23 08:47:22 +0700

    Add nvim extras and adjust keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Do not store to clipboard
+keymap.set("n", "x", '"_x')
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
